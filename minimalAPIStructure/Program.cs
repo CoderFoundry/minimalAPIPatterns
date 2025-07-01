4fab57b945f41ab700a6dbc7b5ac818d4647a2cb
Bobby Davis <bdavis@core-techs.net>
2025-07-01 15:56:39 -0400

    removed commented code

--- minimalAPIStructure/Program.cs
+++ minimalAPIStructure/Program.cs
@@ -59,66 +59,6 @@ app.MapScalarApiReference("", opt =>
     
 });
 
-#region Customer Endpoints
-//// GET /customers
-//app.MapGet("/customers", async (CustomerService svc) =>
-//    TypedResults.Ok(await svc.GetCustomersAsync())
-//)
-//.Produces<IEnumerable<CustomerResponseDTO>>(StatusCodes.Status200OK)
-//.WithName("GetCustomers")
-//.WithTags("Customers");
-
-//// GET /customers/{id}
-//app.MapGet("/customers/{id:int}", async Task<IResult> (int id, CustomerService svc) =>
-//{
-//    var customer = await svc.GetCustomerByIdAsync(id);
-//    return customer is not null
-//        ? TypedResults.Ok(customer)
-//        : TypedResults.NotFound();
-//})
-//.Produces<CustomerResponseDTO>(StatusCodes.Status200OK)
-//.Produces(StatusCodes.Status404NotFound)
-//.WithName("GetCustomerById")
-//.WithTags("Customers");
-
-//// POST /customers
-//app.MapPost("/customers", async (CustomerRequestDTO dto, CustomerService svc) =>
-//{
-//    var created = await svc.AddCustomerAsync(dto);
-//    return TypedResults.Created($"/customers/{created.Id}", created);
-//})
-//.Produces<CustomerResponseDTO>(StatusCodes.Status201Created)
-//.WithName("AddCustomer")
-//.WithTags("Customers");
-
-//// PUT /customers/{id}
-//app.MapPut("/customers/{id:int}", async Task<IResult> (int id, CustomerRequestDTO dto, CustomerService svc) =>
-//{
-//    var updated = await svc.UpdateCustomerAsync(id, dto);
-//    return updated is not null
-//        ? TypedResults.Ok(updated)
-//        : TypedResults.NotFound();
-//})
-//.Produces<CustomerResponseDTO>(StatusCodes.Status200OK)
-//.Produces(StatusCodes.Status404NotFound)
-//.WithName("UpdateCustomer")
-//.WithTags("Customers");
-
-//// DELETE /customers/{id}
-//app.MapDelete("/customers/{id:int}", async Task<IResult> (int id, CustomerService svc) =>
-//{
-//    var deleted = await svc.DeleteCustomerAsync(id);
-//    return deleted
-//        ? TypedResults.NoContent()
-//        : TypedResults.NotFound();
-//})
-//.Produces(StatusCodes.Status204NoContent)
-//.Produces(StatusCodes.Status404NotFound)
-//.WithName("DeleteCustomer")
-//.WithTags("Customers");
-
-#endregion customer endpoints
-
 app.MapCustomerEndpoints();
 app.MapProductEndpoints();
 app.MapOrderEndpoints();
